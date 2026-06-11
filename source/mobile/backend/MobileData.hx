9590fcb8e911882da25f2e2b8697ffcfcd587253
Ethantobot <ethantobot7@gmail.com>
2026-06-11 16:10:55 +0200

    Update MobileData.hx

--- source/mobile/backend/MobileData.hx
+++ source/mobile/backend/MobileData.hx
@@ -12,8 +12,6 @@ class MobileData
 	public static var dpadModes:Map<String, MobileButtonsData> = new Map();
 	public static var hitboxModes:Map<String, CustomHitboxData> = new Map();
 
-	public static var mode(get, set):Int;
-	public static var forcedMode:Null<Int>;
 	public static var save:FlxSave;
 
 	public static function init()
@@ -21,63 +19,22 @@ class MobileData
 		save = new FlxSave();
 		save.bind('MobileControls', CoolUtil.getSavePath());
 
-		readDirectory(Paths.getPreloadPath('mobile/MobileButton/DPadModes'), dpadModes);
-		readDirectory(Paths.getPreloadPath('mobile/Hitbox/HitboxModes'), hitboxModes);
-		readDirectory(Paths.getPreloadPath('mobile/MobileButton/ActionModes'), actionModes);
+		readDirectory(Paths.getSharedPath('mobile/MobileButton/DPadModes'), dpadModes);
+		readDirectory(Paths.getSharedPath('mobile/Hitbox/HitboxModes'), hitboxModes);
+		readDirectory(Paths.getSharedPath('mobile/MobileButton/ActionModes'), actionModes);
 		#if MODS_ALLOWED
-		for (folder in directoriesWithFile(Paths.getPreloadPath(), 'mobile/MobileButton/'))
+		for (folder in Mods.directoriesWithFile(Paths.getSharedPath(), 'mobile/MobileButton/'))
 		{
 			readDirectory(Path.join([folder, 'DPadModes']), dpadModes);
 			readDirectory(Path.join([folder, 'ActionModes']), actionModes);
 		}
-		for (folder in directoriesWithFile(Paths.getPreloadPath(), 'mobile/Hitbox/'))
+		for (folder in Mods.directoriesWithFile(Paths.getSharedPath(), 'mobile/Hitbox/'))
 		{
 			readDirectory(Path.join([folder, 'HitboxModes']), hitboxModes);
 		}
 		#end
 	}
 
-	public static function setMobilePadCustom(mobilePad:MobilePad):Void
-	{
-		if (save.data.buttons == null)
-		{
-			save.data.buttons = new Array();
-			for (buttons in mobilePad)
-				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
-		}
-		else
-		{
-			var tempCount:Int = 0;
-			for (buttons in mobilePad)
-			{
-				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
-				tempCount++;
-			}
-		}
-
-		save.flush();
-	}
-
-	public static function getMobilePadCustom(mobilePad:MobilePad):MobilePad
-	{
-		var tempCount:Int = 0;
-
-		if (save.data.buttons == null)
-			return mobilePad;
-
-		for (buttons in mobilePad)
-		{
-			if (save.data.buttons[tempCount] != null)
-			{
-				buttons.x = save.data.buttons[tempCount].x;
-				buttons.y = save.data.buttons[tempCount].y;
-			}
-			tempCount++;
-		}
-
-		return mobilePad;
-	}
-	
 	static function readDirectory(folder:String, map:Dynamic)
 	{
 		folder = folder.contains(':') ? folder.split(':')[1] : folder;
@@ -96,60 +53,6 @@ class MobileData
 			}
 		}
 	}
-
-	static function directoriesWithFile(path:String, fileToFind:String, mods:Bool = true)
-	{
-		var foldersToCheck:Array<String> = [];
-		#if sys
-		if(FileSystem.exists(path + fileToFind))
-		#end
-			foldersToCheck.push(path + fileToFind);
-
-		#if MODS_ALLOWED
-		if(mods)
-		{
-			// Global mods first
-			for(mod in Paths.getGlobalMods())
-			{
-				var folder:String = Paths.mods(mod + '/' + fileToFind);
-				if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(folder);
-			}
-
-			// Then "PsychEngine/mods/" main folder
-			var folder:String = Paths.mods(fileToFind);
-			if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(Paths.mods(fileToFind));
-
-			// And lastly, the loaded mod's folder
-			if(Paths.currentModDirectory != null && Paths.currentModDirectory.length > 0)
-			{
-				var folder:String = Paths.mods(Paths.currentModDirectory + '/' + fileToFind);
-				if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(folder);
-			}
-		}
-		#end
-		return foldersToCheck;
-	}
-
-	static function set_mode(mode:Int = 3)
-	{
-		save.data.mobileControlsMode = mode;
-		save.flush();
-		return mode;
-	}
-
-	static function get_mode():Int
-	{
-		if (forcedMode != null)
-			return forcedMode;
-
-		if (save.data.mobileControlsMode == null)
-		{
-			save.data.mobileControlsMode = 3;
-			save.flush();
-		}
-
-		return save.data.mobileControlsMode;
-	}
 }
 
 typedef MobileButtonsData =
@@ -165,48 +68,53 @@ typedef CustomHitboxData =
 	single:Array<HitboxData>,
 	double:Array<HitboxData>,
 	triple:Array<HitboxData>,
-	quad:Array<HitboxData>
+	quad:Array<HitboxData>,
+	test:Array<HitboxData>
 }
 
 typedef HitboxData =
 {
 	button:String, // what Hitbox Button should be used, must be a valid Hitbox Button var from Hitbox as a string.
 	//if custom ones isn't setted these will be used
-	x:Float, // the button's X position on screen.
-	y:Float, // the button's Y position on screen.
-	width:Int, // the button's Width on screen.
-	height:Int, // the button's Height on screen.
+	x:Dynamic, // the button's X position on screen.
+	y:Dynamic, // the button's Y position on screen.
+	width:Dynamic, // the button's Width on screen.
+	height:Dynamic, // the button's Height on screen.
 	color:String, // the button color, default color is white.
 	returnKey:String, // the button return, default return is nothing (please don't add custom return if you don't need).
+	extraKeyMode:Null<Int>,
 	//Top
-	topX:Null<Float>,
-	topY:Null<Float>,
-	topWidth:Null<Int>,
-	topHeight:Null<Int>,
+	topX:Dynamic,
+	topY:Dynamic,
+	topWidth:Dynamic,
+	topHeight:Dynamic,
 	topColor:String,
 	topReturnKey:String,
+	topExtraKeyMode:Null<Int>,
 	//Middle
-	middleX:Null<Float>,
-	middleY:Null<Float>,
-	middleWidth:Null<Int>,
-	middleHeight:Null<Int>,
+	middleX:Dynamic,
+	middleY:Dynamic,
+	middleWidth:Dynamic,
+	middleHeight:Dynamic,
 	middleColor:String,
 	middleReturnKey:String,
+	middleExtraKeyMode:Null<Int>,
 	//Bottom
-	bottomX:Null<Float>,
-	bottomY:Null<Float>,
-	bottomWidth:Null<Int>,
-	bottomHeight:Null<Int>,
+	bottomX:Dynamic,
+	bottomY:Dynamic,
+	bottomWidth:Dynamic,
+	bottomHeight:Dynamic,
 	bottomColor:String,
-	bottomReturnKey:String
+	bottomReturnKey:String,
+	bottomExtraKeyMode:Null<Int>
 }
 
 typedef ButtonsData =
 {
 	button:String, // what MobileButton should be used, must be a valid MobileButton var from MobilePad as a string.
-	graphic:String, // the graphic of the button, usually can be located in the MobilePad xml .
+	graphic:String, // the graphic of the button, usually can be located in the MobilePad xml.
 	x:Float, // the button's X position on screen.
 	y:Float, // the button's Y position on screen.
 	color:String, // the button color, default color is white.
-	bg:String // the button background for TouchPad, default background is `bg`.
+	bg:String // the button background for MobilePad, default background is `bg`.
 }
